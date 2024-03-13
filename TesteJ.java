0a74d15693eba7cd88a509c4e3aa095bc2dd3798
Dev-Benicio <benicio.info.t.i@gmail.com>
2024-03-13 17:16:41 -0300

    Envio

---
+++ TesteJ.java
@@ -0,0 +1,9 @@
+package jteste;
+
+public class TesteJ {
+    public static void main( String [] args ) {
+        System.out.println( "Olá Mundo! Benício é o meu Rei!!" );
+        System.out.print("Benício é o meu Rei!!");
+    }
+    
+}
